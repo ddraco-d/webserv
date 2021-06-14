b8daf4d807870f5a90394c175eeb04c018bc0a49
ashea <ashea@student.21-school.ru>
2021-06-14 17:17:20 +0500

    ashea

---
+++ Request.hpp
@@ -0,0 +1,96 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
+
+class Request
+{
+private:
+	std::string method;
+	std::string path;
+	std::string version;
+	std::map<std::string, std::string> headers;
+public:
+	Request(char * buffer);
+	std::string get(void);
+	~Request();
+};
+
+std::vector<std::string> split_line(const std::string &buffer)
+{
+	std::vector<std::string> table;
+	std::stringstream ss;
+	std::string item;
+	ss.str(buffer);
+	while (std::getline(ss, item, '\n'))
+		table.push_back(item);
+	return (table);
+}
+
+std::vector<std::string> split_first(const std::string &buffer)
+{
+	std::vector<std::string> table;
+	std::stringstream ss;
+	std::string item;
+	ss.str(buffer);
+	while (std::getline(ss, item, ' '))
+		table.push_back(item);
+	return (table);
+}
+
+std::vector<std::string> split_headers(const std::string &buffer)
+{
+	std::vector<std::string> table;
+	std::stringstream ss;
+	std::string item;
+	char delim = ':';
+	ss.str(buffer);
+	while (std::getline(ss, item, delim))
+	{
+		std::string::iterator end_pos = std::remove(item.begin(), item.end(), ' ');
+		item.erase(end_pos, item.end());
+		table.push_back(item);
+		delim = '\0';
+	}
+	return (table);
+}
+
+Request::Request(char * buffer)
+{
+	std::vector<std::string> table;
+	std::vector<std::string> first_line;
+	std::vector<std::string> other_line;
+	table = split_line(buffer);
+	if (!table.empty())
+	{
+		first_line = split_first(table[0].data());
+		if (first_line.size() == 3)
+		{
+			method = first_line[0];
+			path = first_line[1];
+			version = first_line[2];
+		}
+		for (int i = 1; i < table.size(); ++i)
+		{
+			other_line = split_headers(table[i].data());
+			if (other_line.size() == 2)
+				headers[other_line[0]] = other_line[1];
+		}
+	}
+}
+std::string Request::get(void) 
+{
+	std::string request;
+	request = "METHOD:" + method + "\n" 
+	+ "PATH:" + path + "\n"
+	+ "VERSION:" + version + "\n";
+
+	for (std::map<std::string, std::string>::iterator it = headers.begin(); it != headers.end(); ++it)
+		request += it->first + ":" + it->second + "\n";
+	return (request);
+}
+ Request::~Request()
+ {
+
+ }
---
+++ web.cpp
@@ -0,0 +1,96 @@
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <fstream>
+
+#include "Request.hpp"
+
+//Создайте сокет int server_fd = socket(домен, тип, протокол);
+//Определите сокет
+//На сервере дождитесь входящего соединения
+//Отправляйте и получайте сообщения
+//Закройте розетку
+/*
+struct sockaddr_in 
+{ 
+    __uint8_t         sin_len; 
+    sa_family_t       sin_family; 
+    in_port_t         sin_port; 
+    struct in_addr    sin_addr; 
+    char              sin_zero[8]; 
+};
+*/
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd;
+	int new_socket;
+	long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_port = htons(PORT);
+	address.sin_addr.s_addr = htonl(INADDR_ANY);
+    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+	
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+	
+	
+	
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        std::vector<char> request(1400);
+        valread = recv(new_socket, request.data(), request.size(), 0);
+        printf("%s\n", request.data());
+		if(valread < 0)
+		{
+			printf("Нет байт для чтения\n");
+		}
+		std::stringstream response, h;
+		response << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: ";
+		std::ifstream html("my.html");
+		h << html.rdbuf();
+		response << h.str().length() << "\n\n" << h.str();
+		send(new_socket , response.str().c_str(), strlen(response.str().c_str()), 0);
+
+		std::stringstream response2;
+		response2 << "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: ";
+		Request req(request.data());
+		response2 << req.get().length() << "\n\n" << req.get();
+		//send(new_socket , response2.str().c_str(), strlen(response2.str().c_str()), 0);
+		//printf("%s", req.get().c_str());
+        printf("-------------------- Message sent-------------------\n");
+        close(new_socket);
+    }
+    return 0;
+}
